af38274d8d1807fb003ebf0dc0031718d6f0a8e3
Roshan Paudel <paudelroshan93@gmail.com>
2025-08-03 19:19:13 +1000

    Routes Created, Login page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import { Button } from "react-bootstrap";
+import { ToastContainer } from "react-toastify";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route index element={<Login />}>
+        <ToastContainer />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
