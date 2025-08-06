53bfe9f3cd92e6034928d32fde3d3ddc7ef651a1
Roshan Paudel <paudelroshan93@gmail.com>
2025-08-06 20:37:26 +1000

    CSS file defined

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { Button } from "react-bootstrap";
 import { ToastContainer } from "react-toastify";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import "./App.css"; // This line imports the CSS file
 
 function App() {
   return (
-    <>
+    <div className="wrapper">
       <Routes>
         <Route path="/" element={<Login />} />
       </Routes>
       <ToastContainer />
-    </>
+    </div>
   );
 }
 
