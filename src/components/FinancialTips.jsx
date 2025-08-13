d3325b170d3605fc733162966a97b4b9bb26dd7e
Roshan Paudel <paudelroshan93@gmail.com>
2025-08-13 11:55:34 +1000

    Financial tips added

--- src/components/FinancialTips.jsx
+++ src/components/FinancialTips.jsx
@@ -1,52 +1,71 @@
 import React from "react";
 const financialTips = [
   {
+    tips: "Focus on value, not just price.",
     quote: "Price is what you pay; value is what you get.",
     author: "Warren Buffett",
   },
   {
+    tips: "Save first, spend later.",
     quote:
       "Do not save what is left after spending, but spend what is left after saving.",
     author: "Warren Buffett",
   },
   {
+    tips: "Manage spending, not just income.",
     quote:
       "It's not your salary that makes you rich; it's your spending habits.",
     author: "Charles A. Jaffe",
   },
   {
+    tips: "Avoid small, wasteful expenses.",
     quote: "Beware of little expenses; a small leak will sink a great ship.",
     author: "Benjamin Franklin",
   },
   {
+    tips: "Control your money, don’t be controlled.",
     quote: "Money is a terrible master but an excellent servant.",
     author: "P.T. Barnum",
   },
   {
+    tips: "Take charge of your finances.",
     quote:
       "You must gain control over your money, or the lack of it will forever control you.",
     author: "Dave Ramsey",
   },
   {
+    tips: "Invest based on value, not price.",
     quote:
       "The stock market is filled with individuals who know the price of everything, but the value of nothing.",
     author: "Philip Fisher",
   },
   {
+    tips: "Learn to grow your wealth.",
     quote: "An investment in knowledge pays the best interest.",
     author: "Benjamin Franklin",
   },
   {
+    tips: "Plan your spending with a budget.",
     quote:
       "A budget is telling your money where to go instead of wondering where it went.",
     author: "John C. Maxwell",
   },
   {
+    tips: "Educate yourself and act for freedom.",
     quote:
       "Financial freedom is available to those who learn about it and work for it.",
     author: "Robert Kiyosaki",
   },
 ];
 export const FinancialTips = () => {
-  return <div>FinancialTips</div>;
+
+  return (
+    <div>
+      <h4>Learn to grow your wealth.</h4>
+      <div className="fw-bolder">
+        " An investment in knowledge pays the best interest. "
+      </div>
+      <div>- Benjamin Franklin</div>
+    </div>
+  );
 };
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,12 +1,15 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { FinancialTips } from "../components/FinancialTips";
 
 const Signup = () => {
   return (
     <Container className="p-5">
-      <Row className="bg-dark p-5">
-        <Col>Tips here</Col>
+      <Row className="bg-dark p-5 rounded">
+        <Col>
+          <FinancialTips />
+        </Col>
         <Col>Form here</Col>
       </Row>
     </Container>
